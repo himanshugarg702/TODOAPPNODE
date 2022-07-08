f2e027a42460cf28f38d0da76ac452c2f5162f81
Himanshu Garg <hg631124@gmail.com>
2022-07-09 00:20:08 +0530

    this is my complete to do app using node js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express=require('express');
 const app=express();
 const port=7000;
+// use express router
+app.use('/',require('./routes'));
 
-
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+app.use(express.static('assets'));
+// app.post('/action/create-task',function(req,res){
+//   console.log(res);
+// })
 app.listen(port,function(err){
     if(err){
         console.log(`Error is running the server ${port}`);
